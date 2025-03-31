a71cde1ad3a78cad25661538241c59861ddff8a0
dwfdee <deeskinnabeats@hotmail.com>
2025-03-31 13:50:07 -0300

    Add project files.

---
+++ TicketHub/Controllers/PaymentInfoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketHub.Properties;
+
+namespace TicketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentInfoController : ControllerBase
+    {
+        private readonly ILogger<PaymentInfoController> _logger;
+
+        public PaymentInfoController(ILogger<PaymentInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]paymentInfo paymentInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid details.");
+            }
+
+            return Ok(paymentInfo);
+        }
+    }
+
+  
+}
